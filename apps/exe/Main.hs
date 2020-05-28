b5b8f5f73fccbc87dc82ad021c0b8e94630aa3af
Andrei Dziahel <develop7@develop7.info>
2020-05-28 18:59:05 +0300

    holy shit, it compiles

--- apps/exe/Main.hs
+++ apps/exe/Main.hs
@@ -1,10 +1,21 @@
 module Main where
 
 import Lib
+import Data.Maybe (fromMaybe)
+import System.Environment (lookupEnv)
+import Database.PostgreSQL.LibPQ (connectdb)
+import Data.ByteString.Char8 (pack)
+import Network.Wai.Handler.Warp (run)
 
 main :: IO ()
 main = do
   port <- fromMaybe "3000" <$> lookupEnv "PORT"
   pgURI <- fromMaybe "postgres:///restograph" <$> lookupEnv "DATABASE_URL"
   
-  runApp (read port) pgURI 
+  runApp (read port) pgURI
+
+runApp :: Int -> String -> IO()
+runApp port pgURI = do
+  conn <- connectdb $ pack pgURI
+  run port (waiApp conn)
+  pure ()
--- src/Lib.hs
+++ src/Lib.hs
@@ -5,34 +5,71 @@
 
 module Lib
   ( someFunc,
+    waiApp,
   )
 where
 
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Proxy (Proxy (..))
 import Data.Text (Text)
+import Database.PostgreSQL.LibPQ (Connection)
 import GHC.Generics
-import Servant.API ((:>), Capture, Delete, DeleteNoContent, Get, JSON, NoContent, Put, PutNoContent, ReqBody, (:<|>))
-import Servant.API.Generic ((:-), ToServantApi)
+import Hasql.Connection ()
+import Network.Wai (Application)
+import Servant.API ((:<|>) (..), (:<|>), (:>), Accept, Capture, Delete, DeleteNoContent, Get, JSON, NoContent (..), Put, PutNoContent, ReqBody)
+import Servant.API.Generic ((:-), ToServantApi, genericApi)
+import Servant.Server (Server, serve)
+import Servant.Server.Generic (AsServer, genericServe, genericServer)
 
-data Node = Node {id :: Int, nodeLabel :: Text} deriving (Generic)
+data Node = Node {nodeId :: Int, nodeLabel :: Text} deriving (Generic)
+
+instance ToJSON Node
 
 data NodeReq = NodeReq {nrLabel :: Text} deriving (Generic)
 
+instance FromJSON NodeReq
+
 type API = "graph" :> GraphAPI
 
-type GraphAPI = "node" :> ToServantApi NodeAPI :<|> "link" :> ToServantApi LinkAPI 
+type GraphAPI = "node" :> ToServantApi NodeAPI :<|> "link" :> ToServantApi LinkAPI
 
-data NodeAPI route
+data NodeAPI r
   = NodeAPI
-      { create :: route :- ReqBody '[JSON] NodeReq :> Put '[JSON] Node,
-        delete :: route :- Capture "id" Int :> DeleteNoContent '[JSON] NoContent,
-        rename :: route :- Capture "id " Int :> "label" :> ReqBody '[JSON] NodeReq :> Put '[JSON] Node,
-        neighbours :: route :- Capture "id " Int :> "neighbours" :> Get '[JSON] [Node]
+      { create :: r :- ReqBody '[JSON] NodeReq :> Put '[JSON] Node,
+        delete :: r :- Capture "id" Int :> DeleteNoContent '[JSON] NoContent,
+        rename :: r :- Capture "id " Int :> "label" :> ReqBody '[JSON] NodeReq :> Put '[JSON] Node,
+        neighbours :: r :- Capture "id " Int :> "neighbours" :> Get '[JSON] [Node],
+        list :: r :- Get '[JSON] [Node]
       }
+  deriving (Generic)
 
-data LinkAPI route
+data LinkAPI r
   = LinkAPI
-      { new :: Capture "id_from" Int :> Capture "id_to" Int :> PutNoContent '[NoContent] NoContent
+      { new :: r :- Capture "id_from" Int :> Capture "id_to" Int :> PutNoContent '[JSON] NoContent
       }
+  deriving (Generic)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+server :: Connection -> Server API
+server conn = genericServer nodeAPI :<|> genericServer linkAPI
+  where
+    nodeAPI :: NodeAPI AsServer
+    nodeAPI =
+      NodeAPI
+        { list = return [Node {nodeId = 10, nodeLabel = "wat"}],
+          create = \NodeReq {nrLabel = nl} -> return $ Node {nodeId = 100, nodeLabel = nl},
+          delete = \_id -> return NoContent,
+          rename = \_id NodeReq {nrLabel = nl} -> return $ Node {nodeId = 42, nodeLabel = nl},
+          neighbours = \_id -> return [Node 14 "Hurr", Node 15 "Durr"]
+        }
+    linkAPI :: LinkAPI AsServer
+    linkAPI = LinkAPI {new = (\fro to -> return NoContent)}
+
+api :: Proxy API
+api = Proxy
+
+waiApp :: Connection -> Application
+waiApp conn = do
+  serve api $ server conn
