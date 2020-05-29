a0198ef4c6fa12d156c77908249e832f2bd78adc
Andrei Dziahel <develop7@develop7.info>
2020-05-29 17:40:40 +0300

    restograph-exe: log requests in dev environment

--- apps/exe/Main.hs
+++ apps/exe/Main.hs
@@ -1,21 +1,27 @@
 module Main where
 
-import Lib
-import Data.Maybe (fromMaybe)
-import System.Environment (lookupEnv)
-import Database.PostgreSQL.LibPQ (connectdb)
 import Data.ByteString.Char8 (pack)
+import Data.Maybe (fromMaybe)
+import Database.PostgreSQL.LibPQ (connectdb)
+import Lib
+import Network.Wai (Middleware)
 import Network.Wai.Handler.Warp (run)
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import System.Environment (lookupEnv)
 
 main :: IO ()
 main = do
   port <- fromMaybe "3000" <$> lookupEnv "PORT"
+  env <- fromMaybe "development" <$> lookupEnv "ENV"
   pgURI <- fromMaybe "postgres:///restograph" <$> lookupEnv "DATABASE_URL"
-  
-  runApp (read port) pgURI
+  let middleware =
+        case env of
+          "development" -> logStdoutDev
+          _ -> id
+  runApp (read port) pgURI middleware
 
-runApp :: Int -> String -> IO()
-runApp port pgURI = do
+runApp :: Int -> String -> Middleware -> IO ()
+runApp port pgURI middleware = do
   conn <- connectdb $ pack pgURI
-  run port (waiApp conn)
+  run port (middleware $ waiApp conn)
   pure ()
