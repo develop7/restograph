84bf9b0a8d3fdc7d5eb622b3f9830468a0eb8fd8
Andrei Dziahel <develop7@develop7.info>
2020-06-01 18:52:53 +0300

    swagger & swagger-ui: up&running

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,44 +1,62 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Lib
-  ( someFunc,
-    waiApp,
+  ( waiApp,
   )
 where
 
-import Data.Aeson (FromJSON, ToJSON)
+import Control.Lens ((&), (.~), (?~), mapped)
+import Data.Aeson (FromJSON, ToJSON, toJSON)
+import qualified Data.Aeson as Aeson (Value (..))
 import Data.Proxy (Proxy (..))
+import Data.Swagger (NamedSchema (..), Swagger (..), SwaggerItems (..), SwaggerType (..), ToSchema (..), defaultSchemaOptions, description, example, genericDeclareNamedSchema, info, items, schema, title, type_, version)
 import Data.Text (Text)
+import Data.Typeable (Typeable)
 import Database.PostgreSQL.LibPQ (Connection)
 import Deriving.Aeson.Stock
 import GHC.Generics
 import Hasql.Connection ()
 import Network.Wai (Application)
-import Servant.API ((:<|>) (..), (:<|>), (:>), Accept, Capture, Delete, DeleteNoContent, Get, JSON, NoContent (..), Put, PutNoContent, ReqBody)
+import Servant.API ((:<|>) (..), (:<|>), (:>), Accept, Capture, Delete, DeleteNoContent, Get, JSON, NoContent (..), PlainText, Put, PutNoContent, ReqBody)
 import Servant.API.Generic ((:-), ToServantApi, genericApi)
 import Servant.Server (Server, serve)
-import Servant.Server.Generic (AsServer, genericServe, genericServer)
+import Servant.Server.Generic (AsServer, genericServer)
+import Servant.Swagger (toSwagger)
+import Servant.Swagger.UI (SwaggerSchemaUI, swaggerSchemaUIServer)
 
 data Node = Node {nodeId :: Int, nodeLabel :: Text}
-  deriving (Generic)
+  deriving (Generic, Typeable)
   deriving (FromJSON, ToJSON) via PrefixedSnake "node" Node
 
+instance ToSchema Node where
+  declareNamedSchema proxy =
+    genericDeclareNamedSchema defaultSchemaOptions proxy
+      & mapped . schema . description ?~ "Graph node"
+      & mapped . schema . example ?~ toJSON (Node 42 "answer")
+
 data NodeReq = NodeReq {nrLabel :: Text}
-  deriving (Generic)
+  deriving (Generic, Typeable)
   deriving (FromJSON, ToJSON) via PrefixedSnake "nr" NodeReq
 
-type API = "graph" :> GraphAPI
+instance ToSchema NodeReq where
+  declareNamedSchema proxy =
+    genericDeclareNamedSchema defaultSchemaOptions proxy
+      & mapped . schema . description ?~ "Node-related request data"
+      & mapped . schema . example ?~ toJSON (NodeReq "answer")
 
-type GraphAPI = "node" :> ToServantApi NodeAPI :<|> "link" :> ToServantApi LinkAPI
+type API = SwaggerSchemaUI "swagger-ui" "swagger.json" :<|> SwaggerAPI :<|> GraphAPI
+
+type GraphAPI = "graph" :> ("node" :> ToServantApi NodeAPI :<|> "link" :> ToServantApi LinkAPI)
 
 data NodeAPI r
   = NodeAPI
       { create :: r :- ReqBody '[JSON] NodeReq :> Put '[JSON] Node,
-        delete :: r :- Capture "id" Int :> DeleteNoContent '[JSON] NoContent,
+        delete :: r :- Capture "id" Int :> DeleteNoContent '[PlainText] NoContent,
         rename :: r :- Capture "id " Int :> "label" :> ReqBody '[JSON] NodeReq :> Put '[JSON] Node,
         neighbours :: r :- Capture "id " Int :> "neighbours" :> Get '[JSON] [Node],
         list :: r :- Get '[JSON] [Node]
@@ -51,11 +69,21 @@ data LinkAPI r
       }
   deriving (Generic)
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+type SwaggerAPI = "swagger.json" :> Get '[JSON] Swagger
+
+graphSwagger :: Swagger
+graphSwagger =
+  toSwagger (Proxy :: Proxy GraphAPI)
+    & info . title .~ "Graph manipulation API"
+    & info . version .~ "0.1"
+    & info . description ?~ "An undirected graph manipulation API"
 
 server :: Connection -> Server API
-server conn = genericServer nodeAPI :<|> genericServer linkAPI
+server conn =
+  swaggerSchemaUIServer graphSwagger 
+    :<|> return graphSwagger
+    :<|> genericServer nodeAPI
+    :<|> genericServer linkAPI
   where
     nodeAPI :: NodeAPI AsServer
     nodeAPI =
