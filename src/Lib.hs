78c74461cd7455261273751d660974b1ccef9a3d
Andrei Dziahel <develop7@develop7.info>
2020-06-02 19:17:23 +0300

    introduced NodeId newtype for Node id

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeOperators, GeneralisedNewtypeDeriving #-}
 
 module Lib
   ( waiApp,
@@ -13,32 +13,36 @@ where
 import Control.Lens ((&), (.~), (?~), mapped)
 import Data.Aeson (toJSON)
 import Data.Proxy (Proxy (..))
-import Data.Swagger (Swagger (..), ToSchema (..), defaultSchemaOptions, description, example, genericDeclareNamedSchema, info, schema, title, version)
+import Data.Swagger (Swagger (..), ToSchema (..), defaultSchemaOptions, description, example, genericDeclareNamedSchema, info, schema, title, version, ToParamSchema)
 import Data.Text (Text)
-import Data.Typeable (Typeable)
 import Database.PostgreSQL.LibPQ (Connection)
 import Deriving.Aeson.Stock
 import Hasql.Connection ()
 import Network.Wai (Application)
-import Servant.API ((:<|>) (..), (:<|>), (:>), Capture, DeleteNoContent, Get, JSON, NoContent (..), PlainText, Put, PutNoContent, ReqBody)
+import Servant.API ((:<|>) (..), (:<|>), (:>), Capture, DeleteNoContent, Get, JSON, NoContent (..), PlainText, Put, PutNoContent, ReqBody, FromHttpApiData)
 import Servant.API.Generic ((:-), ToServantApi)
 import Servant.Server (Server, serve)
 import Servant.Server.Generic (AsServer, genericServer)
 import Servant.Swagger (toSwagger)
 import Servant.Swagger.UI (SwaggerSchemaUI, swaggerSchemaUIServer)
 
-data Node = Node {nodeId :: Int, nodeLabel :: Text}
-  deriving (Generic, Typeable)
+newtype NodeId = NodeId Int deriving (Generic, ToJSON, FromJSON, FromHttpApiData)
+
+instance ToParamSchema NodeId
+instance ToSchema NodeId
+
+data Node = Node {nodeId :: NodeId, nodeLabel :: Text}
+  deriving (Generic)
   deriving (FromJSON, ToJSON) via PrefixedSnake "node" Node
 
 instance ToSchema Node where
   declareNamedSchema proxy =
     genericDeclareNamedSchema defaultSchemaOptions proxy
       & mapped . schema . description ?~ "Graph node"
-      & mapped . schema . example ?~ toJSON (Node 42 "answer")
+      & mapped . schema . example ?~ toJSON (Node (NodeId 42) "answer")
 
 data NodeReq = NodeReq {nrLabel :: Text}
-  deriving (Generic, Typeable)
+  deriving (Generic)
   deriving (FromJSON, ToJSON) via PrefixedSnake "nr" NodeReq
 
 instance ToSchema NodeReq where
@@ -54,16 +58,16 @@ type GraphAPI = "graph" :> ("node" :> ToServantApi NodeAPI :<|> "link" :> ToServ
 data NodeAPI r
   = NodeAPI
       { create :: r :- ReqBody '[JSON] NodeReq :> Put '[JSON] Node,
-        delete :: r :- Capture "id" Int :> DeleteNoContent '[PlainText] NoContent,
-        rename :: r :- Capture "id " Int :> "label" :> ReqBody '[JSON] NodeReq :> Put '[JSON] Node,
-        neighbours :: r :- Capture "id " Int :> "neighbours" :> Get '[JSON] [Node],
+        delete :: r :- Capture "id" NodeId :> DeleteNoContent '[PlainText] NoContent,
+        rename :: r :- Capture "id " NodeId :> "label" :> ReqBody '[JSON] NodeReq :> Put '[JSON] Node,
+        neighbours :: r :- Capture "id " NodeId :> "neighbours" :> Get '[JSON] [Node],
         list :: r :- Get '[JSON] [Node]
       }
   deriving (Generic)
 
 data LinkAPI r
   = LinkAPI
-      { new :: r :- Capture "id_from" Int :> Capture "id_to" Int :> PutNoContent '[JSON] NoContent
+      { new :: r :- Capture "id_from" NodeId :> Capture "id_to" NodeId :> PutNoContent '[JSON] NoContent
       }
   deriving (Generic)
 
@@ -86,11 +90,11 @@ server _conn =
     nodeAPI :: NodeAPI AsServer
     nodeAPI =
       NodeAPI
-        { list = return [Node {nodeId = 10, nodeLabel = "wat"}],
-          create = \NodeReq {nrLabel = nl} -> return $ Node {nodeId = 100, nodeLabel = nl},
+        { list = return [Node {nodeId = NodeId 10, nodeLabel = "wat"}],
+          create = \NodeReq {nrLabel = nl} -> return $ Node {nodeId = NodeId 100, nodeLabel = nl},
           delete = \_id -> return NoContent,
-          rename = \_id NodeReq {nrLabel = nl} -> return $ Node {nodeId = 42, nodeLabel = nl},
-          neighbours = \_id -> return [Node 14 "Hurr", Node 15 "Durr"]
+          rename = \_id NodeReq {nrLabel = nl} -> return $ Node {nodeId = NodeId 42, nodeLabel = nl},
+          neighbours = \_id -> return [Node (NodeId 14) "Hurr", Node (NodeId 15) "Durr"]
         }
     linkAPI :: LinkAPI AsServer
     linkAPI = LinkAPI {new = \_fro _to -> return NoContent}
